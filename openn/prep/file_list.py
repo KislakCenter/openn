734cb475bbd8654881ab0229c65434a6a889280c
Doug Emery <emeryr@upenn.edu>
2014-06-09 17:43:04 -0400

    Add FileList.FileData nested class

--- openn/prep/file_list.py
+++ openn/prep/file_list.py
@@ -1,24 +1,85 @@
 import json
+import os
 
 class FileList:
+    DOCUMENT   = 'document'
+    EXTRA      = 'extra'
+    FILE_TYPES = ( DOCUMENT, EXTRA )
+
+    """
+    Wrapper for JSON file list expected by CommonPrep. Input has the
+    format below.
+
+        {
+            "document": [
+                {
+                    "filename": "data/mscodex1223_wk1_front0001.tif",
+                    "label": "Front cover"
+                },
+                {
+                    "filename": "data/mscodex1223_wk1_front0002.tif",
+                    "label": "Inside front cover"
+                },
+                {
+                    "filename": "data/mscodex1223_wk1_front0003.tif",
+                    "label": "[Flyleaf 1 recto]"
+                },
+                {
+                    "filename": "data/mscodex1223_wk1_front0004.tif",
+                    "label": "[Flyleaf 1 verso]"
+                },
+                {
+                    "filename": "data/mscodex1223_wk1_body0001.tif",
+                    "label": "1r"
+                },
+                {
+                    "filename": "data/mscodex1223_wk1_body0002.tif",
+                    "label": "1v"
+                },
+                // ....
+                {
+                    "filename": "data/mscodex1223_wk1_back0005.tif",
+                    "label": "Spine"
+                }
+            ],
+            "extra": [
+                {
+                    "filename": "data/mscodex1589_test ref1_1.tif"
+                }
+            ]
+        }
+    """
     def __init__(self, file_list_path):
         self.file_list_path = file_list_path
-        self.file_list = json.load(open(file_list_path))
+        file_dict = json.load(open(file_list_path))
+        self.file_list = {}
+        for file_type in FileList.FILE_TYPES:
+            self.file_list[file_type] = []
+            for fdata in file_dict.get(file_type, []):
+                self.file_list[file_type].append(self.FileData(fdata))
 
-    def files(self,type='document'):
+    def files(self,type=DOCUMENT):
         return self.file_list[type]
 
     @property
     def document_files(self):
-        return self.files('document')
+        return self.files(FileList.DOCUMENT)
 
-    def count(self,type='document'):
+    def count(self,type=DOCUMENT):
         return len(self.files(type))
 
-    def file(self,index,type='document'):
+    def file(self,index,type=DOCUMENT):
         lst = self.files(type)
         return lst[index] if (lst and len(lst) > index) else None
 
-    def filename(self,index,type='document'):
-        file = self.file(index,type)
-        return file['filename'] if file else None
+    def filename(self,index,type=DOCUMENT):
+        if self.file(index,type):
+            return self.file(index,type).filename
+
+
+    class FileData:
+        def __init__(self, dict):
+            self.filename = dict.get('filename')
+            self.label = dict.get('label')
+            self.derivs = dict.get('derivs', {})
+
--- openn/tests/test_file_list.py
+++ openn/tests/test_file_list.py
@@ -15,6 +15,7 @@ class TestFileList(TestCase):
 
 
     test_file_list   = os.path.join(settings.PROJECT_PATH, 'test/data/json/mscodex1223_file_list.json')
+    test_no_extras   = os.path.join(settings.PROJECT_PATH, 'test/data/json/mscodex1223_no_extra.json')
 
     def setUp(self):
         pass
@@ -24,6 +25,28 @@ class TestFileList(TestCase):
 
     def test_init(self):
         self.assertIsInstance(FileList(TestFileList.test_file_list), FileList)
+
+    def test_file_lists(self):
+        lst = FileList(TestFileList.test_file_list)
+        self.assertIsInstance(lst.files(FileList.DOCUMENT), list)
+        self.assertIsInstance(lst.files(FileList.EXTRA), list)
+
+    def test_no_extras_has_empty_list(self):
+        lst = FileList(TestFileList.test_no_extras)
+        self.assertIsInstance(lst.files(FileList.EXTRA), list)
+
+    def test_document_count_not_zero(self):
+        self.assertGreater(FileList(TestFileList.test_file_list).count(), 0)
+
+    def test_extras_count_not_zero(self):
+        self.assertGreater(FileList(TestFileList.test_file_list).count(FileList.EXTRA), 0)
+
+    def test_extras_count_is_zero(self):
+        self.assertEqual(FileList(TestFileList.test_no_extras).count(FileList.EXTRA), 0)
+
+    def test_file_data_is_correct_type(self):
+        self.assertIsInstance(FileList(TestFileList.test_file_list).files()[0], FileList.FileData)
+
 
 if __name__ == '__main__':
     unittest.main()
