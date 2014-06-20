49b947480accb4c3a3d162989b97ec9e10a54e53
Doug Emery <emeryr@upenn.edu>
2014-06-20 17:15:26 -0400

    Move namespaces to OPennTEI.ns property

--- openn/xml/openn_tei.py
+++ openn/xml/openn_tei.py
@@ -1,12 +1,17 @@
 from lxml import etree
 
 class OPennTEI:
+    TEI_NS = 'http://www.tei-c.org/ns/1.0'
+    
     def __init__(self, tei_path, mode='r'):
         self.tei = etree.parse(open(tei_path, mode))
+        self._namespaces = { 't': OPennTEI.TEI_NS }
+
+    @property
+    def ns(self):
+        return self._namespaces
 
     @property
     def call_number(self):
-        return self.tei.xpath('//t:msIdentifier/t:idno', 
-                namespaces={ 't': 'http://www.tei-c.org/ns/1.0' })[0].text
-
-
+        xpath = '//t:msIdentifier/t:idno'
+        return self.tei.xpath(xpath, namespaces=self.ns)[0].text
