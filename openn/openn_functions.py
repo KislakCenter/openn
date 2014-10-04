e63ebaaf70c305eb1216d78e7b3b68a6800b64c5
Doug Emery <emeryr@upenn.edu>
2014-10-04 17:39:15 -0400

    Move get_class to openn/openn_functions.py

--- openn/openn_functions.py
+++ openn/openn_functions.py
@@ -1,8 +1,16 @@
 import time
 
+def get_class(kls):
+    parts = kls.split('.')
+    module = ".".join(parts[:-1])
+    m = __import__( module )
+    for comp in parts[1:]:
+        m = getattr(m, comp)
+    return m
+
 def tstamp():
     return time.strftime('%Y%m%dT%H%M%S')
-    
+
 def str_time():
     return time.strftime('%Y-%m-%dT%H:%M:%S')
 
