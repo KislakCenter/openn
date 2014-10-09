90e91e7e3c0661cc82e6ac4bbe619d8626872e4e
Doug Emery <emeryr@upenn.edu>
2014-10-09 18:50:10 -0400

    Change image_type field size (from 1 => 20)

---
+++ openn/migrations/0011_auto__chg_field_image_image_type.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Image.image_type'
+        db.alter_column(u'openn_image', 'image_type', self.gf('django.db.models.fields.CharField')(max_length=20))
+
+    def backwards(self, orm):
+
+        # Changing field 'Image.image_type'
+        db.alter_column(u'openn_image', 'image_type', self.gf('django.db.models.fields.CharField')(max_length=1))
+
+    models = {
+        u'openn.derivative': {
+            'Meta': {'ordering': "['deriv_type']", 'object_name': 'Derivative'},
+            'bytes': ('django.db.models.fields.IntegerField', [], {}),
+            'deriv_type': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '20'}),
+            'height': ('django.db.models.fields.IntegerField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.related.ForeignKey', [], {'default': 'None', 'to': u"orm['openn.Image']"}),
+            'path': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '255'}),
+            'width': ('django.db.models.fields.IntegerField', [], {})
+        },
+        u'openn.document': {
+            'Meta': {'ordering': "['collection', 'base_dir', 'call_number']", 'unique_together': "(('collection', 'base_dir'),)", 'object_name': 'Document'},
+            'base_dir': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '30'}),
+            'call_number': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '255'}),
+            'collection': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '30'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_online': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'tei_file_name': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '40', 'null': 'True', 'blank': 'True'}),
+            'tei_xml': ('django.db.models.fields.TextField', [], {'default': 'None', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.TextField', [], {'default': 'None'}),
+            'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        u'openn.image': {
+            'Meta': {'ordering': "('order',)", 'object_name': 'Image'},
+            'document': ('django.db.models.fields.related.ForeignKey', [], {'default': 'None', 'to': u"orm['openn.Document']"}),
+            'filename': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '255'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image_type': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'label': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '255'}),
+            'order': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
+        }
+    }
+
+    complete_apps = ['openn']
