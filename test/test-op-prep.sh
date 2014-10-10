430850ef53d4c3d9c1760fbd98024340c9e8dac9
Doug Emery <emeryr@upenn.edu>
2014-10-10 15:37:23 -0400

    Ensure clean setup and teardown

--- test/test-op-prep.sh
+++ test/test-op-prep.sh
@@ -4,6 +4,9 @@ DATA_DIR=`dirname $0`/data
 SHUNIT_HELPER=`dirname $0`/shunit_helper
 
 setUp() {
+    if [ ! -d $TEST_STAGING_DIR ]; then
+        mkdir $TEST_STAGING_DIR
+    fi
     # make sure the database is empty
     for table in openn_derivative openn_image openn_document
     do
@@ -16,7 +19,7 @@ tearDown() {
     do
         mysql -u openn openn -e "delete from $table"
     done
-    rm -rf $TEST_STAGING_DIR/*
+    rm -rf $TEST_STAGING_DIR/* 2>/dev/null
 }
 
 testRun() {
