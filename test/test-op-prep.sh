3cc468dfc493c6634d6ccaa4dc2db858f390c1ee
Doug Emery <emeryr@upenn.edu>
2014-10-13 14:49:39 -0400

    extend op-prep tests

--- test/test-op-prep.sh
+++ test/test-op-prep.sh
@@ -24,10 +24,25 @@ tearDown() {
 }
 
 testRun() {
-    cp -r $TEST_DATA_DIR/mscodex1223 $TEST_STAGING_DIR/mscodex1223
-    op-prep medren $TEST_STAGING_DIR/mscodex1223
+    source_dir=$TEST_STAGING_DIR/mscodex1223
+    cp -r $TEST_DATA_DIR/mscodex1223 $source_dir
+    op-prep medren $source_dir
+    status=$?
+    assertEquals 0 $status
+    assertTrue "Expected TEI file in $source_dir/data; found: `ls $source_dir/data 2>/dev/null`" "ls $source_dir/data/*[0-9]_TEI.xml"
+    assertFalse "Should not find PIH XML in $source_dir found: `ls $source_dir/pih*.xml 2>/dev/null`" "ls $source_dir/pih*.xml"
+    assertFalse "Should not find file_list.json in $source_dir; found: `ls $source_dir/*.json 2>/dev/null`" "ls $source_dir/*.json"
+    assertFalse "Should find PARTIAL_TEI.xml in $source_dir; found `ls $source_dir/PARTIAL_TEI.xml 2>/dev/null`" "ls $source_dir/PARTIAL_TEI.xml"
 }
 
+testImagesNotInPIH() {
+
+    package_dir=$TEST_STAGING_DIR/mscodex1589
+    cp -r $DIR_EXTRA_IMAGES $package_dir
+    op-prep medren $package_dir
+    status=$?
+    assertEquals 0 $status
+}
 
 # Run shunit
 . $shunit
