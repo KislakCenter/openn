f930cfb3af11d1b9c9c7802daea1d0e3fee7ca95
Doug Emery <emeryr@upenn.edu>
2014-10-17 07:56:16 -0400

    Remove commented code

--- openn/xml/openn_tei.py
+++ openn/xml/openn_tei.py
@@ -104,17 +104,3 @@ class OPennTEI(XMLWhatsit):
                 graphic = etree.Element('graphic', **attrs)
                 surface.append(graphic)
             facs.append(surface)
-
-    # def to_string(self):
-    #     return etree.tostring(self.xml, pretty_print=True, xml_declaration=True, encoding='UTF-8')
-
-    # def _get_attr(self,xpath,attr):
-    #     nodes = self._get_nodes(xpath)
-    #     return nodes[0].get(attr) if len(nodes) > 0 else None
-
-    # def _get_text(self,xpath):
-    #     nodes = self._get_nodes(xpath)
-    #     return nodes[0].text if len(nodes) > 0 else None
-
-    # def _get_nodes(self,xpath):
-    #     return self.xml.xpath(xpath, namespaces=self.ns)
