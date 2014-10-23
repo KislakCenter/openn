89b6878da0504d6e18a5e48178450d0094515a96
Doug Emery <emeryr@upenn.edu>
2014-10-23 12:08:01 -0400

    Add db options init_command

--- openn/settings.py
+++ openn/settings.py
@@ -18,6 +18,8 @@ DATABASES = {
             'USER': os.environ['OPENN_DB_USER'],
             'PASSWORD': os.environ['OPENN_DB_PASSWORD'],
             'HOST': os.environ['OPENN_DB_HOST'],
+            'OPTIONS': {
+                'init_command': 'SET storage_engine=INNODB,character_set_connection=utf8,collation_connection=utf8_unicode_ci',
             }
         }
 
