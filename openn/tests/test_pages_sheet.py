4c520167243e840d95c3bbde1ae8d92e9fd76164
Doug Emery <emeryr@upenn.edu>
2015-06-04 15:34:43 -0400

    Test pages sheet data, format validation

--- openn/tests/test_pages_sheet.py
+++ openn/tests/test_pages_sheet.py
@@ -20,6 +20,7 @@ class TestPagesSheet(TestCase):
 
     helen_griffith_workbook  = os.path.join(diaries_dir, 'bryn_mawr/HelenGriffith_Diary.xlsx')
     pages_workbook           = os.path.join(sheets_dir, 'pages.xlsx')
+    pages_invalid_workbook   = os.path.join(sheets_dir, 'pages_invalid.xlsx')
 
     pages_config = {
         'description': {
@@ -84,6 +85,7 @@ class TestPagesSheet(TestCase):
                     'required': False,
                     'repeating': True,
                     'data_type': 'string',
+                    'value_list': [ 'TOC1', 'TOC2', 'TOC3', 'ILL', 'BLANK' ],
                     'blank': {
                         'if': {
                             'field': 'file_name',
@@ -113,6 +115,7 @@ class TestPagesSheet(TestCase):
                     'required': False,
                     'repeating': True,
                     'data_type': 'string',
+                    'value_list': [ 'TOC1', 'TOC2', 'TOC3', 'ILL', 'BLANK' ],
                     'blank': {
                         'if': {
                             'field': 'file_name',
@@ -142,6 +145,7 @@ class TestPagesSheet(TestCase):
                     'required': False,
                     'repeating': True,
                     'data_type': 'string',
+                    'value_list': [ 'TOC1', 'TOC2', 'TOC3', 'ILL', 'BLANK' ],
                     'blank': {
                         'if': {
                             'field': 'file_name',
@@ -171,6 +175,7 @@ class TestPagesSheet(TestCase):
                     'required': False,
                     'repeating': True,
                     'data_type': 'string',
+                    'value_list': [ 'TOC1', 'TOC2', 'TOC3', 'ILL', 'BLANK' ],
                     'blank': {
                         'if': {
                             'field': 'file_name',
@@ -222,3 +227,62 @@ class TestPagesSheet(TestCase):
         sheet.validate()
         if len(sheet.errors) > 0: self.pp(sheet.errors)
         self.assertEqual(len(sheet.errors), 0)
+
+    def test_required_if_other_nonempty(self):
+        sheet = OPWorkbook(self.pages_invalid_workbook, self.pages_config).pages
+        sheet.validate_requirement('serial_num')
+        self.assertEqual(len(sheet.errors), 1)
+        self.assertRegexpMatches(sheet.errors[0], r'"SERIAL_NUM" cannot be empty.* "FILE_NAME"')
+
+    def test_empty_if_other_nonempty(self):
+        sheet = OPWorkbook(self.pages_invalid_workbook, self.pages_config).pages
+        sheet.validate_blank('serial_num')
+        self.assertEqual(len(sheet.errors), 1)
+        self.assertRegexpMatches(sheet.errors[0], r'"SERIAL_NUM" must be empty.* "FILE_NAME"')
+
+    def test_value1_blank_if_toc1_empty(self):
+        sheet = OPWorkbook(self.pages_invalid_workbook, self.pages_config).pages
+        sheet.validate_blank('value1')
+        self.assertEqual(len(sheet.errors), 1)
+        self.assertRegexpMatches(sheet.errors[0], r'"VALUE1" must be empty.* "TAG1".*"September 24"')
+
+    def test_value2_blank_if_toc2_empty(self):
+        sheet = OPWorkbook(self.pages_invalid_workbook, self.pages_config).pages
+        sheet.validate_blank('value2')
+        self.assertEqual(len(sheet.errors), 1)
+        self.assertRegexpMatches(sheet.errors[0], r'"VALUE2" must be empty.* "TAG2".*"September 27"')
+
+    def test_value3_blank_if_toc3_empty(self):
+        sheet = OPWorkbook(self.pages_invalid_workbook, self.pages_config).pages
+        sheet.validate_blank('value3')
+        self.assertEqual(len(sheet.errors), 1)
+        self.assertRegexpMatches(sheet.errors[0], r'"VALUE3" must be empty.* "TAG3".*"March 15"')
+
+    def test_value4_blank_if_toc4_empty(self):
+        sheet = OPWorkbook(self.pages_invalid_workbook, self.pages_config).pages
+        sheet.validate_blank('value4')
+        self.assertEqual(len(sheet.errors), 1)
+        self.assertRegexpMatches(sheet.errors[0], r'"VALUE4" must be empty.* "TAG4".*"Car"')
+
+    def test_required_based_on_value_list_tag1_value1(self):
+        sheet = OPWorkbook(self.pages_invalid_workbook, self.pages_config).pages
+        sheet.validate_requirement('value1')
+        self.assertEqual(len(sheet.errors), 1)
+        self.assertRegexpMatches(sheet.errors[0], r'"VALUE1" cannot be empty.* "TAG1".*"TOC1"')
+
+    def test_required_based_on_value_list_tag2_value2(self):
+        sheet = OPWorkbook(self.pages_invalid_workbook, self.pages_config).pages
+        sheet.validate_requirement('value2')
+        self.assertEqual(len(sheet.errors), 1)
+        self.assertRegexpMatches(sheet.errors[0], r'"VALUE2" cannot be empty.* "TAG2".*"ILL"')
+
+    def test_notrequired_based_on_value_list_tag4_value4(self):
+        sheet = OPWorkbook(self.pages_invalid_workbook, self.pages_config).pages
+        sheet.validate_requirement('value4')
+        self.assertEqual(len(sheet.errors), 0)
+
+    def test_required_based_on_value_list_tag3_value3(self):
+        sheet = OPWorkbook(self.pages_invalid_workbook, self.pages_config).pages
+        sheet.validate_requirement('value3')
+        self.assertEqual(len(sheet.errors), 1)
+        self.assertRegexpMatches(sheet.errors[0], r'"VALUE3" cannot be empty.* "TAG3".*"TOC3"')
