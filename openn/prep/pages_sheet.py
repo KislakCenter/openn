881878c3d2a059d024a08ce888c929cedfb26a99
Doug Emery <emeryr@upenn.edu>
2015-06-04 13:57:26 -0400

    Stub check_file_list() method

--- openn/prep/pages_sheet.py
+++ openn/prep/pages_sheet.py
@@ -19,6 +19,7 @@ class PagesSheet(ValidatableSheet):
     def validate(self):
         self.check_required_headings()
         self.check_pages_values()
+        self.check_file_list()
 
     def check_required_headings(self):
         for field in self.required_fields:
@@ -29,3 +30,7 @@ class PagesSheet(ValidatableSheet):
     def check_pages_values(self):
         for attr in self.fields:
             self.validate_field(attr)
+
+    def check_file_list(self):
+        # TODO implement check_file_list
+        pass
