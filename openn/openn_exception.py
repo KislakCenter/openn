302cd3fbfec61a1d200b2eb77431ce91ec074d96
Doug Emery <emeryr@upenn.edu>
2015-07-15 10:50:33 -0400

    Handle stacktrace of causing exception

--- openn/openn_exception.py
+++ openn/openn_exception.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
+import traceback
 
 class OPennException(Exception):
-    def __init__(self, message, cause=None):
+    def __init__(self, message, cause=None, cause_text=None):
         if cause is None:
             super(OPennException, self).__init__(message)
         else:
             super(OPennException, self).__init__(message + u', caused by ' + repr(cause))
         self.cause = cause
+        self.cause_text = cause_text
+
+    def print_cause(self):
+        print self.cause_text
