558d42dfc4c3c4874b0048bd19c3c65bc616ed60
Doug Emery <emeryr@upenn.edu>
2015-10-04 21:05:13 -0400

    Fix openn.tests.test_document_data

--- openn/tests/test_document_data.py
+++ openn/tests/test_document_data.py
@@ -9,9 +9,12 @@ from django.conf import settings
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../..'))
 from openn.models import *
 from openn.pages.document_data import DocumentData
+from openn.collections.configs import Configs
 
 class TestDocumentData(TestCase):
     fixtures = [ 'test.json' ]
+    configs = Configs(settings.COLLECTIONS)
+    pennmss_wrapper = configs.get_collection('pennmss')
 
     def setUp(self):
         pass
@@ -21,17 +24,17 @@ class TestDocumentData(TestCase):
 
     def test_pages(self):
         document = Document.objects.get(base_dir='mscodex1589')
-        doc_data = DocumentData(document)
+        doc_data = DocumentData(document, self.pennmss_wrapper, 'html')
         self.assertTrue(len(doc_data.pages) > 0)
 
     def test_ms_item_pages(self):
         document = Document.objects.get(base_dir='mscodex1589')
-        doc_data = DocumentData(document)
+        doc_data = DocumentData(document, self.pennmss_wrapper, 'html')
         # print "doc_data.ms_item_pages: %d" % len(doc_data.ms_item_pages)
         self.assertTrue(len(doc_data.ms_item_pages) > 0)
 
     def test_deco_note_pages(self):
         document = Document.objects.get(base_dir='mscodex1589')
-        doc_data = DocumentData(document)
+        doc_data = DocumentData(document, self.pennmss_wrapper, 'html')
         # print "doc_data.deco_note_pages: %d" % len(doc_data.deco_note_pages)
         self.assertTrue(len(doc_data.deco_note_pages) > 0)
