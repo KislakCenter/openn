363b6273016db3699632a201091b5fc37649caa4
Doug Emery <emeryr@upenn.edu>
2015-12-18 13:23:17 -0500

    Custom prep and prep class for Archie, Galen

---
+++ bin/op-cust-prep.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python
+
+"""Module summary
+
+Usage: op-cust-prep.py COLL_PREP FOLDER_NAME INPUT_FILE
+
+- add the document with FOLDER_NAME using COLL_PREP and the text of
+  INPUT_FILE
+
+"""
+
+# optparse is both easy to use and produces clean code
+# the main optparse docs can be found here:
+# http://docs.python.org/library/optparse.html
+# there's a much better tutorial that works you through optparse
+# starting with a simple example and slowly adding complexity.
+from optparse import OptionParser
+from datetime import datetime
+import os
+import sys
+import logging
+import shutil
+
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openn.settings")
+
+from openn.openn_exception import OPennException
+from openn.prep.prep_setup import PrepSetup
+from openn.prep.prep_config_factory import PrepConfigFactory
+from openn.collections.configs import Configs
+
+
+# Import your models for use in your script
+from openn.models import *
+from openn.openn_functions import *
+# database access functions
+from openn import openn_db
+from django.core import serializers
+from django.conf import settings
+
+class OpOptParser(OptionParser):
+    def format_epilog(self, formatter):
+        return self.epilog
+
+def cmd():
+    return os.path.basename(__file__)
+
+def setup_logger():
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    fmt = '%(asctime)s - %(name)-15s - %(levelname)s - %(message)s'
+    formatter = logging.Formatter(fmt)
+    ch.setFormatter(formatter)
+    logging.getLogger().addHandler(ch)
+    logging.getLogger().setLevel(logging.DEBUG)
+    global logger
+    logger = logging.getLogger(__name__)
+
+def collection_configs():
+    return Configs(settings.COLLECTIONS)
+
+def validate_doc(base_dir, openn_collection, opts):
+    params = { 'base_dir': base_dir,
+               'openn_collection': openn_collection }
+    if openn_db.doc_exists(params):
+        if opts.clobber:
+            pass
+        else:
+            msg = "Document already exists with base_dir '%s' "
+            msg += "and primary collection '%s'"
+            raise OPennException(msg % (base_dir, openn_collection))
+    else:
+        if opts.clobber:
+            msg = "Clobber option not valid for non-existent document with"
+            msg += " base_dir '%s' and primary collection '%s'"
+            raise OPennException(msg % (base_dir, openn_collection))
+
+def setup_prepstatus(doc):
+    # destroy the associate prep if it exists
+    if hasattr(doc, 'prepstatus'):
+        prepstatus = doc.prepstatus
+        prepstatus.delete()
+    prepstatus = PrepStatus(document=doc)
+    prepstatus.save()
+    return prepstatus
+
+def set_success_status(prepstatus):
+    prepstatus.finished  = datetime.now()
+    prepstatus.succeeded = True
+    prepstatus.save()
+
+def set_failure_status(prepstatus, ex):
+    prepstatus.finished  = datetime.now()
+    prepstatus.succeeded = False
+    prepstatus.error     = str(ex)
+    prepstatus.save()
+
+def get_prep_config(prep_config_tag, folder_name, input_file):
+    prep_config_factory = PrepConfigFactory(
+        prep_configs_dict=settings.PREP_CONFIGS,
+        prep_methods=settings.PREPARATION_METHODS,
+        collection_configs=settings.COLLECTIONS,
+        prep_context=settings.PREP_CONTEXT)
+
+    return prep_config_factory.create_prep_config(prep_config_tag)
+
+def do_prep(prep_config_tag, folder_name, input_file, opts):
+    try:
+        prepstatus       = None
+        base_dir         = os.path.basename(folder_name)
+        prep_config      = get_prep_config(prep_config_tag, folder_name, input_file)
+        openn_collection = prep_config.openn_collection()
+        validate_doc(base_dir, openn_collection, opts)
+
+        setup            = PrepSetup()
+        coll             = prep_config.collection()
+        doc              = setup.prep_document(coll, base_dir)
+
+        prepstatus       = setup_prepstatus(doc)
+
+        prep_class       = prep_config.get_prep_class()
+        cust_prep        = prep_class(doc, input_file)
+
+        cust_prep.prep()
+        set_success_status(prepstatus)
+        return doc
+    except Exception as ex:
+        if prepstatus is not None:
+            set_failure_status(prepstatus, ex)
+        raise
+
+def main(cmdline=None):
+    """op-cust-prep main
+    """
+    status = 0
+
+    setup_logger()
+
+    parser = make_parser()
+
+    opts, args = parser.parse_args(cmdline)
+
+    if opts.list_coll_preps:
+        print "%s" % ('\n'.join(sorted(prep_config_tags())),)
+        return status
+
+    if len(args) != 3:
+        parser.error('Wrong number of arguments')
+
+    try:
+        prep_config_tag  = args[0]
+        folder_name      = args[1]
+        input_file       = args[2]
+
+        do_prep(prep_config_tag, folder_name, input_file, opts)
+
+    except OPennException as ex:
+        status = 4
+        print_exc()
+        msg = str(ex)
+        print msg
+
+    return status
+
+
+def make_parser():
+    """op-cust-prep.py option parser"""
+
+    usage = "%prog [OPTIONS] COLL_PREP FOLDER_NAME INPUT_FILE"
+    epilog = """ Prepare FOLDER_NAME as an OPenn document using collection prep
+COLL_PREP with descriptive metadata from INPUT_FILE.
+
+Known collection preps are:
+
+    %s
+
+FOLDER_NAME is a path or base directory name to use this document. The
+name should correspond to the folder name on OPenn.
+
+Update: Note that this function has not yet been implemented.
+
+Resume: Resume will fail if the source directory does not have a
+`status.txt` file.
+
+Clobber: Use clobber to replace an existing document that did not
+prepare correctly the first time. Will fail if document is on-line.
+
+""" % ('\n    '.join(sorted(prep_config_tags())),)
+    # usage = "%prog COLLECTION SOURCE_DIR"
+
+    parser = OpOptParser(usage=usage,epilog=epilog)
+
+    list_help = 'List all known collection preps and quit; no'
+    list_help += ' arguments required; any arguments ignored'
+    parser.add_option('-l', '--list',
+                      action='store_true', dest='list_coll_preps', default=False,
+                      help=list_help)
+
+    clobber_help = 'if it exists, delete document from database'
+    parser.add_option('-x', '--clobber',
+                      action='store_true', dest='clobber', default=False,
+                      help=clobber_help)
+
+    return parser
+
+if __name__ == "__main__":
+    # this runs when the application is run from the command
+    # it grabs sys.argv[1:] which is everything after the program name
+    # and passes it to main
+    # the return value from main is then used as the argument to
+    # sys.exit, which you can test for in the shell.
+    # program exit codes are usually 0 for ok, and non-zero for something
+    # going wrong.
+    sys.exit(main(sys.argv[1:]))
---
+++ openn/prep/dirless_tei_prep.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+import logging
+import sys
+
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')))
+from openn.openn_exception import OPennException
+from openn.openn_functions import *
+from openn.xml.openn_tei import OPennTEI
+from openn.prep.prep_setup import PrepSetup
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openn.settings")
+from openn.models import *
+from openn import openn_db
+from django.core import serializers
+from django.conf import settings
+
+class DirlessTEIPrep(object):
+
+    logger = logging.getLogger(__name__)
+
+    def __init__(self, doc, tei_file):
+        """Create a new TEI for the given PrepConfig 'prep_config',
+        'folder_name', and 'tei_file'.
+        """
+        self._doc         = doc
+        self._tei_file    = tei_file
+
+    def validate(self):
+        self._validate_tei()
+
+    def prep(self):
+        self.validate()
+
+        # set up the document
+        self._doc.tei_xml = self._read_tei()
+        openn_tei = self._openn_tei()
+        self._doc.title = openn_tei.title
+        self._doc.call_number = openn_tei.call_number
+        self._doc.save()
+
+    def _read_tei(self):
+        return open(self._tei_file).read()
+
+    def _openn_tei(self):
+        return OPennTEI(self._read_tei())
+
+    def _validate_tei(self):
+        # Confirm the TEI file exists
+        print "=== tei file is %s ===" % (self._tei_file,)
+        if not os.path.exists(self._tei_file):
+            raise OPennException("Cannot find TEI file: %s", (self._tei_file,))
+
+        # Validate the TEI?
+        #   - Has values: title
+        #   -
+        #   -
+        openn_tei = self._openn_tei()
+
+        errors = []
+        if openn_tei.title is None:
+            errors.append("Title cannot be blank")
+        if openn_tei.licences is None or len(openn_tei.licences) == 0:
+            errors.append("At least one licence must be provided")
+        if len(errors) > 0:
+            msg = "TEI validation errors found: %s"
+            raise OPennException(msg % (' ;'.join(errors),))
