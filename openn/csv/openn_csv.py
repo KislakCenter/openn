21ddd63cbf96472bb9fbf3d0716dd7d50594d0e8
Doug Emery <emeryr@upenn.edu>
2017-01-05 16:48:24 -0500

    Document some methods.

--- openn/csv/openn_csv.py
+++ openn/csv/openn_csv.py
@@ -5,7 +5,19 @@ from openn.csv.unicode_csv import UnicodeCSVWriter
 import openn.openn_functions as opfunc
 
 class OPennCSV(object):
-    """docstring for OPennCSV"""
+    """
+
+    Base class for generating CSV a file. Child classes should override the
+    methods write_file(),  is_makeable(), and is_needed(), as appropropiate.
+
+    Typical usage:
+
+        csv = OPennCSV('path/to/dir', 'file.csv')
+        csv.writerow(['a', 'b', 'c'])
+        csv.writerow([1, 2, 3])
+        csv.close() # important; close the output file object
+
+    """
     def __init__(self, outdir, filename):
         self.outdir    = outdir
         self.filename  = filename
@@ -27,12 +39,36 @@ class OPennCSV(object):
             self.writerow(row)
 
     def close(self):
+    """
+    If output file object exists and is not closed, close it; otherwise,
+    do nothing.
+    """
+
         if self.csvfile is None:
             return
         if self.csvfile.closed:
             return
 
         self.csvfile.close()
+
+    def is_makeable(self):
+        """Always return True. Method should be overridden by
+            implementing class."""
+
+        return True
+
+    def write_file(self):
+        """ Implementing classes should override this method.
+        """
+        pass
+
+    def is_needed(self):
+        """ Return False if is_makeable() returns false. Otherwise, return
+        True. Implmenting classes should override. """
+        if not self.is_makeable():
+            return False
+
+        return True
 
     def _csvfile(self):
         if self.csvfile is None:
