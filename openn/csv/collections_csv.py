8225c9fc8094a74e27c84925ae86d4f3dd09133f
Doug Emery <emeryr@upenn.edu>
2017-01-06 12:49:59 -0500

    Correct OPENN-TEI to TEI

--- openn/csv/collections_csv.py
+++ openn/csv/collections_csv.py
@@ -7,10 +7,10 @@ class CollectionsCSV(OPennCSV):
     """Generate CSV table of contents for all collections. Looks like this (without padding):
 
     collection_id,  collection_tag, collection_type,  metadata_type,  collection_name
-    0001,           ljs,            repository,       OPENN-TEI,      Lawrence J. Schoenberg Manuscripts
-    0002,           pennmss,        repository,       OPENN-TEI,      University of Pennsylvania Books & Manuscripts
-    0003,           brynmawr,       repository,       OPENN-TEI,      Bryn Mawr College Library Special Collections
-    0004,           drexarc,        repository,       OPENN-TEI,      Drexel University Archives and Special Collections
+    0001,           ljs,            repository,       TEI,            Lawrence J. Schoenberg Manuscripts
+    0002,           pennmss,        repository,       TEI,            University of Pennsylvania Books & Manuscripts
+    0003,           brynmawr,       repository,       TEI,            Bryn Mawr College Library Special Collections
+    0004,           drexarc,        repository,       TEI,            Drexel University Archives and Special Collections
     N/A,            bibliophilly,   curated,          N/A,            Bibliotheca Philadelphiensis
     """
     HEADER = 'collection_id,collection_tag,collection_type,metadata_type,collection_name'.split(',')
