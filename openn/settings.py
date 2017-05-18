ae524cf225143af8fcef02c3dbf225c2f1c628ee
Jessica Dummer <jdummer@upenn.edu>
2017-05-18 10:24:51 -0400

    updated Huntington Page with more info and updated settings.py blurb

--- openn/settings.py
+++ openn/settings.py
@@ -500,13 +500,9 @@ movement.""",
 libraries in the United States in its fields of specialization. The
 Library’s collection of rare books, manuscripts, prints, photographs,
 maps, and other materials in the fields of British and American history
-and literature totals more than nine million items. The Library
-collections date from the Middle Ages to the 21st century. The greatest
-concentration is in the English Renaissance, about 1500 to 1641; other
-strengths include medieval manuscripts, incunabula (books printed before
-1501), maps, travel literature, British and American history and
-literature, the American Southwest, and the history of science, medicine
-and technology.""",
+and literature totals more than nine million items. OPenn hosts one item
+from The Huntington Library, the *Autobiography of Benjamin
+Franklin,(Autograph manuscript signed), 1771-1789.*""",
             'include_file': 'HuntingtonLibrary.html',
         },
         {
